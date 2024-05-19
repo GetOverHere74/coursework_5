a0f5662964b543ba3e6b3d319b4c8b6e1878a576
skyl1 <skyl1.maxla2@mail.ru>
2024-05-19 21:17:16 +0500

    Написан абстрактный класс

--- src/api.py
+++ src/api.py
@@ -1,17 +1,26 @@
+import requests
 from abc import ABC, abstractmethod
 
 
 class BaseAPI(ABC):
+    """Абстрактный класс для API запроса"""
     @abstractmethod
     def get_vacancies(self, keyword, count):
         pass
 
 
 class HHApi(BaseAPI):
+    """Класс для работы с API hh.ru"""
     def __init__(self):
-        pass
+        self.url = "https://api.hh.ru/vacancies"
+        self.params = {
+                       'text': '',
+                       'page': 0,
+                       'per_page': 100
+        }
 
     def get_vacancies(self, keyword, count):
-        pass
+        self.params.update({'text': keyword})
+        response = requests.get(self.url, params=self.params)
 
 my_api = HHApi().get_vacancies(user_input, user_count)
