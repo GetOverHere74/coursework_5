ec36e2d9945673d46a509d38e49f9b38cf70f04a
skyl1 <skyl1.maxla2@mail.ru>
2024-05-20 16:08:18 +0500

    Добавлены методы фильтрации вакансий

--- src/worker.py
+++ src/worker.py
@@ -2,9 +2,11 @@ import json
 import os
 from abc import ABC, abstractmethod
 from config import DATA_PATH
+from src.vacancy import Vacancy
 
 
 class BaseWorker(ABC):
+    """Абстрактный класс для работы с вакансиями"""
     @abstractmethod
     def add_vacancy(self, vacancy):
         pass
@@ -14,10 +16,15 @@ class BaseWorker(ABC):
         pass
 
     @abstractmethod
+    def add_vacancies(self, vacancies):
+        pass
+
+    @abstractmethod
     def select_vacancy(self, keyword):
         pass
 
 class JSONWorker(BaseWorker):
+    """Класс для работы с вакансиями в формате JSON"""
     def __init__(self, file_name):
         self.file_path = os.path.join(DATA_PATH, file_name)
         self.prepare()
@@ -28,13 +35,29 @@ class JSONWorker(BaseWorker):
                 json.dump([], file)
 
     def add_vacancy(self, vacancy):
-        pass
+        with open(self.file_path, 'r+', encoding='utf-8') as file:
+            data = json.load(file)
+            data.append(vars(vacancy))
+            file.seek(0)
+            json.dump(data, file, ensure_ascii=False, indent=4)
 
     def add_vacancies(self, vacancies):
-        pass
+        with open(self.file_path, 'r+', encoding='utf-8') as file:
+            data = json.load(file)
+            for vacancy in vacancies:
+                data.append(vars(vacancy))
+            file.seek(0)
+            json.dump(data, file, ensure_ascii=False, indent=4)
 
     def del_vacancy(self, vacancy):
-        pass
+        with open(self.file_path, 'r+', encoding='utf-8') as file:
+            data = json.load(file)
+            data = [v for v in data if v != vars(vacancy)]
+            file.seek(0)
+            json.dump(data, file, ensure_ascii=False, indent=4)
 
     def select_vacancy(self, keyword):
-        pass
+        with open(self.file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            filtered_vacancies = [v for v in data if keyword.lower() in v['title'].lower() or keyword.lower() in v['requirements'].lower() or keyword.lower() in v['responsibility'].lower()]
+            return [Vacancy(**v) for v in filtered_vacancies]
